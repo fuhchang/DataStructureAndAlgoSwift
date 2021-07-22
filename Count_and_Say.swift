e72e224583a3e41980d04ff307c8bc22d2f76dec
Loi Fuh Chang <fuh_chang@Hotmail.com>
2021-07-22 21:53:20 +0800

    add Count and Say

---
+++ Count_and_Say.swift
@@ -0,0 +1,62 @@
+/*
+The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
+
+countAndSay(1) = "1"
+countAndSay(n) is the way you would "say" the digit string from countAndSay(n-1), which is then converted into a different digit string.
+To determine how you "say" a digit string, split it into the minimal number of groups so that each group is a contiguous section all of the same character. Then for each group, say the number of characters, then say the character. To convert the saying into a digit string, replace the counts with a number and concatenate every saying.
+
+For example, the saying and conversion for digit string "3322251":
+
+
+Given a positive integer n, return the nth term of the count-and-say sequence.
+
+ 
+
+Example 1:
+
+Input: n = 1
+Output: "1"
+Explanation: This is the base case.
+Example 2:
+
+Input: n = 4
+Output: "1211"
+Explanation:
+countAndSay(1) = "1"
+countAndSay(2) = say "1" = one 1 = "11"
+countAndSay(3) = say "11" = two 1's = "21"
+countAndSay(4) = say "21" = one 2 + one 1 = "12" + "11" = "1211"
+ 
+
+Constraints:
+
+1 <= n <= 30
+*/
+
+class Solution {
+    func countAndSay(_ n: Int) -> String {
+        var res = "1"
+        for _ in 1..<n {
+            res = countAndSay(res)
+        }
+        return res
+    }
+    
+    func countAndSay(_ s: String) -> String {
+        var res = ""
+        var count = 0
+        var prev: Character?
+        for c in s {
+            if let prev = prev, c != prev && count > 0 {
+                res += "\(count)\(prev)"
+                count = 0
+            }
+            count += 1
+            prev = c
+        }
+        if let prev = prev, count > 0 {
+            res += "\(count)\(prev)"
+        }
+        return res
+    }
+}
