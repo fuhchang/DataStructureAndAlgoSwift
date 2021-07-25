41fbcdc9464daa21687089df29fd2e1829b18a72
Loi Fuh Chang <fuh_chang@Hotmail.com>
2021-07-25 15:39:00 +0800

    update spiral matrix 2

--- Spiral_Matrix_II.swift
+++ Spiral_Matrix_II.swift
@@ -20,51 +20,41 @@ Constraints:
 */
 class Solution {
     func generateMatrix(_ n: Int) -> [[Int]] {
-        var res = [[Int]](repeating: [Int](repeating: 0, count: n), count: n)
-        var k = 1
-        let n2 = n*n
-        
-        var r = 0
-        var c = 0
-        
-        while k <= n2 {
-            // go right
-            while c < n && res[r][c] == 0 {
-                res[r][c] = k
-                k += 1
-                c += 1
+     var result = [[Int]](repeating: [Int](repeating: 0, count: n), count:  n)
+        var top = 0
+        var btm = n - 1
+        var left = 0
+        var right = n - 1
+        var counter  = 1
+        var direction = 0
+        while left <= right && top <= btm {
+            if direction == 0 { // right
+                for i in left...right {
+                    result[top][i] = counter
+                    counter += 1
+                }
+                top += 1
+            } else if direction == 1 {
+                for i in top...btm {
+                    result[i][right] = counter
+                    counter += 1
+                }
+                right -= 1
+            } else if direction == 2 {
+                for i in stride(from: right, through: left, by: -1) {
+                    result[btm][i] = counter
+                    counter += 1
+                }
+                btm -= 1
+            } else {
+                for i in stride(from: btm, through: top, by: -1) {
+                    result[i][left] = counter
+                    counter += 1
+                }
+                left += 1
             }
-            c -= 1
-            r += 1
-            
-            // go down
-            while r < n && res[r][c] == 0 {
-                res[r][c] = k
-                k += 1
-                r += 1
-            }
-            r -= 1
-            c -= 1
-            
-            // go left
-            while c >= 0 && res[r][c] == 0 {
-                res[r][c] = k
-                k += 1
-                c -= 1
-            }
-            c += 1
-            r -= 1
-            
-            // go up
-            while r >= 0 && res[r][c] == 0 {
-                res[r][c] = k
-                k += 1
-                r -= 1
-            }
-            r += 1
-            c += 1
+            direction = (direction + 1) % 4
         }
-        
-        return res
+        return result
     }
 }
