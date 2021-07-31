0cebdec98cc92083cac61ed3b5e3126710844ee4
Loi Fuh Chang <fuh_chang@Hotmail.com>
2021-07-31 11:56:46 +0800

    add 101. Symmetric Tree

---
+++ Remove_Duplicates_from_Sorted_List.swift
@@ -0,0 +1,51 @@
+/*
+Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,1,2]
+Output: [1,2]
+Example 2:
+
+
+Input: head = [1,1,2,3,3]
+Output: [1,2,3]
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 300].
+-100 <= Node.val <= 100
+The list is guaranteed to be sorted in ascending order.
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func deleteDuplicates(_ head: ListNode?) -> ListNode? {
+        var prev: ListNode?
+        var curr: ListNode? = head
+        var next: ListNode? 
+        while curr != nil {
+            if prev?.val == curr?.val {
+               prev?.next = curr?.next
+               curr = prev?.next
+           } else  {
+               next = curr?.next
+               prev = curr
+               curr = next
+           } 
+        }
+        return head
+    }
+}
