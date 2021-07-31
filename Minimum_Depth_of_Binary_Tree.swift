35b8c3acdad7ec195884ce7e7c48e7f1b455a152
Loi Fuh Chang <fuh_chang@Hotmail.com>
2021-07-31 12:57:23 +0800

    add 111. Minimum Depth of Binary Tree

---
+++ Minimum_Depth_of_Binary_Tree.swift
@@ -0,0 +1,51 @@
+/*
+Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+Note: A leaf is a node with no children.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 2
+Example 2:
+
+Input: root = [2,null,3,null,4,null,5,null,6]
+Output: 5
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 105].
+-1000 <= Node.val <= 1000
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func minDepth(_ root: TreeNode?) -> Int {
+        guard let root = root else { return 0 }
+       return helper(root, 1)
+    }
+    func helper(_ node: TreeNode?, _ level: Int) -> Int {
+        guard let node = node else { return Int.max }
+        if node.left == nil && node.right == nil { return level }
+        return min(helper(node.left, level + 1), helper(node.right, level + 1))
+    }
+}
