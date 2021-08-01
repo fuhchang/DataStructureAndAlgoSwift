79b1191c5c58b1d0d8a99f0cd98a24bda7b95fb1
Loi Fuh Chang <fuh_chang@Hotmail.com>
2021-08-01 19:25:37 +0800

    add 73. Set Matrix Zeroes

---
+++ Set_Matrix_Zeroes.swift
@@ -0,0 +1,52 @@
+/*
+Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's, and return the matrix.
+
+You must do it in place.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[1,0,1],[0,0,0],[1,0,1]]
+Example 2:
+
+
+Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[0].length
+1 <= m, n <= 200
+-231 <= matrix[i][j] <= 231 - 1
+*/
+class Solution {
+    func setZeroes(_ matrix: inout [[Int]]) {
+        if matrix.isEmpty { return }
+        
+        let h = matrix.count
+        let w = matrix[0].count
+        var rows = Set<Int>()
+        var cols = Set<Int>()
+        
+        for i in 0..<h {
+            for j in 0..<w {
+                if matrix[i][j] == 0 {
+                    rows.insert(i)
+                    cols.insert(j)
+                }
+            }
+        }
+        for i in 0..<h {
+            for j in 0..<w {
+                if rows.contains(i) || cols.contains(j) {
+                    matrix[i][j] = 0
+                }
+            }
+        }
+    }
+}
