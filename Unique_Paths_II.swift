b88c6d78b0e54ba00c1ba2c66d2d1d0610c14f2c
Loi Fuh Chang <fuh_chang@Hotmail.com>
2021-08-01 17:14:28 +0800

    add 63. Unique Paths II

---
+++ Unique_Paths_II.swift
@@ -0,0 +1,50 @@
+/*
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+Now consider if some obstacles are added to the grids. How many unique paths would there be?
+
+An obstacle and space is marked as 1 and 0 respectively in the grid.
+
+ 
+
+Example 1:
+
+
+Input: obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+Output: 2
+Explanation: There is one obstacle in the middle of the 3x3 grid above.
+There are two ways to reach the bottom-right corner:
+1. Right -> Right -> Down -> Down
+2. Down -> Down -> Right -> Right
+Example 2:
+
+
+Input: obstacleGrid = [[0,1],[0,0]]
+Output: 1
+ 
+
+Constraints:
+
+m == obstacleGrid.length
+n == obstacleGrid[i].length
+1 <= m, n <= 100
+obstacleGrid[i][j] is 0 or 1.
+*/
+class Solution {
+    func uniquePathsWithObstacles(_ obstacleGrid: [[Int]]) -> Int {
+         let m = obstacleGrid.count
+        let n = obstacleGrid[0].count
+        var board = Array(repeating: Array(repeating: 0, count: n + 1), count: m + 1)
+        board[0][1] = 1
+        for i in 1...m {
+            for j in 1...n {
+                if obstacleGrid[i-1][j-1] != 1 {
+                    board[i][j] = board[i-1][j] + board[i][j-1]
+                }
+            }
+        }
+        return board[m][n]
+    }
+}
