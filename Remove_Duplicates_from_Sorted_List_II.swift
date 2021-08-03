285c2fd4b9f55b6a308c50257305a1951c412e21
Loi Fuh Chang <fuh_chang@Hotmail.com>
2021-08-03 20:52:04 +0800

    add 82. Remove Duplicates from Sorted List II

---
+++ Remove_Duplicates_from_Sorted_List_II.swift
@@ -0,0 +1,54 @@
+/*
+Given the head of a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list. Return the linked list sorted as well.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,3,4,4,5]
+Output: [1,2,5]
+Example 2:
+
+
+Input: head = [1,1,1,2,3]
+Output: [2,3]
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 300].
+-100 <= Node.val <= 100
+The list is guaranteed to be sorted in ascending order.
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func deleteDuplicates(_ head: ListNode?) -> ListNode? {
+       if head == nil || head?.next == nil {
+           return head
+       }
+        var curr = head
+        var shouldRemoveCurr = false
+        while curr?.val == curr?.next?.val {
+            curr = curr?.next
+            shouldRemoveCurr = true
+        }
+        let next = deleteDuplicates(curr?.next)
+        if shouldRemoveCurr {
+            return next
+        } else {
+            var curr = curr
+            curr?.next = next
+            return curr
+        }
+    }
+}
