5d0ecd25fe6a894feb210f8e8f98ac4d88eaf09a
Loi Fuh Chang <fuh_chang@Hotmail.com>
2021-08-10 20:45:07 +0800

    add 144. Binary Tree Preorder Traversa

---
+++ Binary_Tree_Preorder_Traversal.swift
@@ -0,0 +1,71 @@
+/*
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+Example 4:
+
+
+Input: root = [1,2]
+Output: [1,2]
+Example 5:
+
+
+Input: root = [1,null,2]
+Output: [1,2]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func preorderTraversal(_ root: TreeNode?) -> [Int] {
+        var path = [Int]()
+        var queue = [TreeNode]()
+        guard let validRoot = root else {
+            return path
+        }
+        queue.append(validRoot)
+        while !queue.isEmpty {
+            let node = queue.removeLast()
+            path.append(node.val)
+            if let rightNode = node.right {
+                queue.append(rightNode)
+            }
+            if let leftNode = node.left {
+                queue.append(leftNode)
+            }
+        }
+        return path
+    }
+}
