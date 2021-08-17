5d26208a44c2c10c54a99130c58669754d9fa743
Loi Fuh Chang <fuh_chang@Hotmail.com>
2021-08-17 21:14:43 +0800

    add 107. Binary Tree Level Order Traversal II

---
+++ Binary_Tree_Level_Order_Traversal_II.swift
@@ -0,0 +1,69 @@
+/*
+Given the root of a binary tree, return the bottom-up level order traversal of its nodes' values. (i.e., from left to right, level by level from leaf to root).
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[15,7],[9,20],[3]]
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
+Example 3:
+
+Input: root = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 2000].
+-1000 <= Node.val <= 1000
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func levelOrderBottom(_ root: TreeNode?) -> [[Int]] {
+        guard let node = root else {
+            return []
+        }
+        var result: [[Int]] = []
+        var values: [Int] = []
+        var queue: [TreeNode?] = [root]
+        queue.append(nil)
+        while !queue.isEmpty {
+            guard let node = queue.removeFirst() else  {
+                result.append(values)
+                if !queue.isEmpty {
+                    values.removeAll()
+                    queue.append(nil)
+                }
+                continue
+            }
+            values.append(node.val)
+            if let left = node.left {
+                queue.append(left)
+            }
+            if let right = node.right {
+                queue.append(right)
+            }
+        }
+        return result.reversed()
+    }
+}
