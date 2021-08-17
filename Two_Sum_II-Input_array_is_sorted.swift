4710557f26498a3545a48e0bca4c531c56344656
Loi Fuh Chang <fuh_chang@Hotmail.com>
2021-08-17 22:13:42 +0800

    add 167. Two Sum II - Input array is sorted

---
+++ Two_Sum_II-Input_array_is_sorted.swift
@@ -0,0 +1,62 @@
+/*
+Given an array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number.
+
+Return the indices of the two numbers (1-indexed) as an integer array answer of size 2, where 1 <= answer[0] < answer[1] <= numbers.length.
+
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+ 
+
+Example 1:
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+Example 2:
+
+Input: numbers = [2,3,4], target = 6
+Output: [1,3]
+Example 3:
+
+Input: numbers = [-1,0], target = -1
+Output: [1,2]
+ 
+
+Constraints:
+
+2 <= numbers.length <= 3 * 104
+-1000 <= numbers[i] <= 1000
+numbers is sorted in non-decreasing order.
+-1000 <= target <= 1000
+The tests are generated such that there is exactly one solution.
+*/
+//Dict
+func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
+	var numberToIndex = [Int: Int]()
+	for (index, number) in numbers.enumerated() {
+		if let other = numberToIndex[target - number] {
+			return [other + 1, index + 1]
+		} else if numberToIndex[number] == nil {
+			numberToIndex[number] = index
+		}
+	}
+
+	return []
+}
+
+//2 pointer
+
+func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
+	var left = 0, right = numbers.count - 1
+	while left < right {
+		if numbers[left] + numbers[right] == target {
+			return [left + 1, right + 1]
+		} else if numbers[left] + numbers[right] < target {
+			left += 1
+		} else {
+			right -= 1
+		}
+	}
+
+	return []
+}
