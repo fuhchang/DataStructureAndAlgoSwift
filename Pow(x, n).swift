d51b3f32b9da683a1d31ce66f8106354129749a0
Loi Fuh Chang <fuh_chang@Hotmail.com>
2021-08-24 14:18:49 +0800

    fix  50. Pow(x, n)

--- Pow(x, n).swift
+++ Pow(x, n).swift
@@ -26,16 +26,15 @@ Constraints:
 */
 class Solution {
     func myPow(_ x: Double, _ n: Int) -> Double {
-        var ans = 1.0
-        var x = x
-        var n = n
-        if n < 0 {
-            x = 1.0/x
-            n = -n
+        var x = x, n = n, res = 1.0
+        
+        if n < 0 { x = 1.0/x ; n = -n }
+        
+        while n > 0 {
+            if n % 2 != 0 { res *= x }
+            x *= x
+            n /= 2
         }
-        for i in 0..<n {
-            ans *= x
-        }
-        return ans
+        return res
     }
 }
