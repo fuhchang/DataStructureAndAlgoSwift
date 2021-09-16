088992cc1a39ea9d9b2bb5a8e2abaf03c0a5f619
Loi Fuh Chang <fuh_chang@Hotmail.com>
2021-09-16 20:01:35 +0800

    add 171. Excel Sheet Column Numbe

---
+++ String/Excel_Sheet_Column_Number.swift
@@ -0,0 +1,44 @@
+/*
+Given a string columnTitle that represents the column title as appear in an Excel sheet, return its corresponding column number.
+
+For example:
+
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
+ 
+
+Example 1:
+
+Input: columnTitle = "A"
+Output: 1
+Example 2:
+
+Input: columnTitle = "AB"
+Output: 28
+Example 3:
+
+Input: columnTitle = "ZY"
+Output: 701
+Example 4:
+
+Input: columnTitle = "FXSHRXW"
+Output: 2147483647
+ 
+
+Constraints:
+
+1 <= columnTitle.length <= 7
+columnTitle consists only of uppercase English letters.
+columnTitle is in the range ["A", "FXSHRXW"].
+*/
+class Solution {
+    func titleToNumber(_ columnTitle: String) -> Int {
+        return columnTitle.unicodeScalars.reduce(0) { $0 * 26 + Int($1.value) - 64}
+    }
+}
