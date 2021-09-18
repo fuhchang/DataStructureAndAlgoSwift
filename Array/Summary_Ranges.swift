6231a269b320b45554f1b984f4921698be1bc911
Loi Fuh Chang <fuh_chang@Hotmail.com>
2021-09-18 13:17:20 +0800

    add 228. Summary Ranges

---
+++ Array/Summary_Ranges.swift
@@ -0,0 +1,76 @@
+/*
+You are given a sorted unique integer array nums.
+
+Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+Each range [a,b] in the list should be output as:
+
+"a->b" if a != b
+"a" if a == b
+ 
+
+Example 1:
+
+Input: nums = [0,1,2,4,5,7]
+Output: ["0->2","4->5","7"]
+Explanation: The ranges are:
+[0,2] --> "0->2"
+[4,5] --> "4->5"
+[7,7] --> "7"
+Example 2:
+
+Input: nums = [0,2,3,4,6,8,9]
+Output: ["0","2->4","6","8->9"]
+Explanation: The ranges are:
+[0,0] --> "0"
+[2,4] --> "2->4"
+[6,6] --> "6"
+[8,9] --> "8->9"
+Example 3:
+
+Input: nums = []
+Output: []
+Example 4:
+
+Input: nums = [-1]
+Output: ["-1"]
+Example 5:
+
+Input: nums = [0]
+Output: ["0"]
+ 
+
+Constraints:
+
+0 <= nums.length <= 20
+-231 <= nums[i] <= 231 - 1
+All the values of nums are unique.
+nums is sorted in ascending order.
+*/
+class Solution {
+    func summaryRanges(_ nums: [Int]) -> [String] {
+        if nums.count == 0
+        {
+            return [String]()
+        }
+        if nums.count == 1
+        {
+            return [String(nums[0])]
+        }
+        var result = [String]()
+        var i = 0
+        while i < nums.count {
+            var a = nums[i]
+            while i+1 < nums.count && (nums[i+1] - nums[i] == 1) {
+   i += 1             
+            }
+            if a != nums[i] {
+                result.append("\(a)->\(nums[i])")
+            } else {
+                result.append("\(a)")
+            }
+            i += 1
+        }
+        return result
+    }
+}
