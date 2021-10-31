5eba83dd8b2a07ce17e786cb69bfc56785b99e15
Loi Fuh Chang <fuh_chang@Hotmail.com>
2021-10-31 13:12:11 +0800

    add 303. Range Sum Query - Immutable 349. Intersection of Two Arrays

---
+++ Array/Intersection_of_Two_Arrays.swift
@@ -0,0 +1,34 @@
+/*
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Explanation: [4,9] is also accepted.
+ 
+
+Constraints:
+
+1 <= nums1.length, nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 1000
+*/
+class Solution {
+    func intersection(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
+        var list1 = nums1.count > nums2.count ? nums2 : nums1
+        var list2 = nums1.count > nums2.count ? nums1 : nums2
+        var result: [Int] = []
+        for num in list1 {
+            if list2.contains(num) && !result.contains(num) {
+                result.append(num)
+            }
+        }
+        return result
+    }
+}
